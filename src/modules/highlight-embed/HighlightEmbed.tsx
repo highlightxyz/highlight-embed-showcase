16df03608f2e1eb16600e226ffd7cdbe5e51300d
bartosjiri <me@bartosjiri.com>
2024-08-06 21:16:31 +0200

    Add Highlight embed

--- src/modules/highlight-embed/HighlightEmbed.tsx
+++ src/modules/highlight-embed/HighlightEmbed.tsx
@@ -1,28 +1,9 @@
-import Button from "../../lib/button/Button";
-
 import { useHighlightEmbedStore } from "./HighlightEmbed.store";
-import { useNftStore } from "../nft/Nft.store";
-
-import { nextOrchestratorStage } from "../orchestrator/Orchestrator.helpers";
 
 import styles from "./HighlightEmbed.module.scss";
 
 const HighlightEmbed = () => {
   const { isEmbedVisible } = useHighlightEmbedStore();
-  const { addToken } = useNftStore();
-
-  // @DEBUG
-  const addDebugTokens = () => {
-    Array.from({ length: 3 }).forEach((_, index) => {
-      addToken({
-        tokenId: String(index),
-        metadata: {
-          image: `/images/nft_example${index + 1}.png`,
-        },
-      });
-    });
-    nextOrchestratorStage();
-  };
 
   return (
     <div
@@ -32,7 +13,12 @@ const HighlightEmbed = () => {
         pointerEvents: isEmbedVisible ? "auto" : "none",
       }}
     >
-      <Button onClick={() => addDebugTokens()}>Demo mint</Button>
+      <div
+        data-widget="highlight-mint-card"
+        data-mint-collection-id="<YOUR-COLLECTION-ID-HERE>"
+        data-template="no-art"
+        data-theme={`{"bgColor":"#2B62F2","textColor":"#ffffff","accentColor":"#d1e2f6", "fontFamily":"Mansalva"}`}
+      ></div>
     </div>
   );
 };
